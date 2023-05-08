cc5d6e21b23cbf620ddecccbb2813d789e186fab
Jake152 <jakepeterson1233@gmail.com>
2023-05-07 23:40:16 -0500

    Implemented MainContainer Component

---
+++ my-app/src/Components/MainContainer/MainContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BasicCalculator from '../BasicCalculator/BasicCalulator';
+
+import './MainContainer.css'
+
+export default function MainContainer() {
+    return (
+        <div id='main-container'>
+            <BasicCalculator />
+        </div>
+    )
+}
