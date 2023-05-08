ca9f96a6374a5a97b07ecbb725302c68c50bddea
Jake152 <jakepeterson1233@gmail.com>
2023-05-08 00:49:09 -0500

    Using handleChange function when btn is clicked in ButtonContainer

--- my-app/src/Components/BasicCalculator/ButtonContainer/ButtonContainer.jsx
+++ my-app/src/Components/BasicCalculator/ButtonContainer/ButtonContainer.jsx
@@ -4,37 +4,37 @@ import Button from '../../../Common/Button/Button';
 
 import './ButtonContainer.css'
 
-export default function ButtonContainer() {
+export default function ButtonContainer({ handleChange, handleClearInput, handlePositiveNegative }) {
     return (
         <div id='btn-container'>
             <Row id='basic-calculator-row-1' className='basic-calculator-row'>
-                <Button id='clear-all-btn' label='Clear' onClick={console.log('Clear all btn clicked')}/>
-                <Button id='postive-negative-btn' label='+/-' onClick={console.log('+/- btn clicked')}/>
-                <Button id='percentage-btn' label='%' onClick={console.log('percentage btn clicked')}/>
-                <Button id='divide-btn' label='/' onClick={console.log('divide btn clicked')}/>
+                <Button id='clear-all-btn' label='Clear' onClick={() => handleClearInput(0)}/>
+                <Button id='postive-negative-btn' label='+/-' onClick={() => handlePositiveNegative()}/>
+                <Button id='percentage-btn' label='%' onClick={() => handleChange('%')}/>
+                <Button id='divide-btn' label='/' onClick={() => handleChange('/')}/>
             </Row>
             <Row id='basic-calculator-row-2' className='basic-calculator-row'>
-                <Button id='num-seven-btn' label='7' onClick={console.log('7 btn clicked')}/>
-                <Button id='num-eight-btn' label='8' onClick={console.log('8 btn clicked')}/>
-                <Button id='num-nine-btn' label='9' onClick={console.log('9 btn clicked')}/>
-                <Button id='multiplication-btn' label='X' onClick={console.log('Multiplication btn clicked')}/>
+                <Button id='num-seven-btn' label='7' onClick={() => handleChange(7)}/>
+                <Button id='num-eight-btn' label='8' onClick={() => handleChange(8)}/>
+                <Button id='num-nine-btn' label='9' onClick={() => handleChange(9)}/>
+                <Button id='multiplication-btn' label='X' onClick={() => handleChange('X')}/>
             </Row>
             <Row id='basic-calculator-row-3' className='basic-calculator-row'>
-                <Button id='num-four-btn' label='4' onClick={console.log('4 btn clicked')}/>
-                <Button id='num-five-btn' label='5' onClick={console.log('5 btn clicked')}/>
-                <Button id='num-six-btn' label='6' onClick={console.log('6 btn clicked')}/>
-                <Button id='subtraction-btn' label='-' onClick={console.log('Subtraction btn clicked')}/>
+                <Button id='num-four-btn' label='4' onClick={() => handleChange(4)}/>
+                <Button id='num-five-btn' label='5' onClick={() => handleChange(5)}/>
+                <Button id='num-six-btn' label='6' onClick={() => handleChange(6)}/>
+                <Button id='subtraction-btn' label='-' onClick={() => handleChange('-')}/>
             </Row>
             <Row id='basic-calculator-row-4' className='basic-calculator-row'>
-                <Button id='num-one-btn' label='1' onClick={console.log('1 btn clicked')}/>
-                <Button id='num-two-btn' label='2' onClick={console.log('2 btn clicked')}/>
-                <Button id='num-three-btn' label='3' onClick={console.log('3 btn clicked')}/>
-                <Button id='addition-btn' label='+' onClick={console.log('Addition btn clicked')}/>
+                <Button id='num-one-btn' label='1' onClick={() => handleChange(1)}/>
+                <Button id='num-two-btn' label='2' onClick={() => handleChange(2)}/>
+                <Button id='num-three-btn' label='3' onClick={() => handleChange(3)}/>
+                <Button id='addition-btn' label='+' onClick={() => handleChange('+')}/>
             </Row>
             <Row id='basic-calculator-row-5' className='basic-calculator-row'>
-                <Button id='num-0-btn' label='0' onClick={console.log('0 btn clicked')}/>
-                <Button id='period-btn' label='.' onClick={console.log('Period btn clicked')}/>
-                <Button id='equals-btn' label='=' onClick={console.log('Equals btn clicked')}/>
+                <Button id='num-0-btn' label='0' onClick={() => handleChange(0)}/>
+                <Button id='period-btn' label='.' onClick={() => handleChange('.')}/>
+                <Button id='equals-btn' label='=' onClick={() => handleChange('=')}/>
             </Row>
         </div>
     )
