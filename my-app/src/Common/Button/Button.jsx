670e182044fd01ed3262c6ee872b7cbb1136ad7d
Jake152 <jakepeterson1233@gmail.com>
2023-05-07 23:38:54 -0500

    Implemented Button Component

---
+++ my-app/src/Common/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Button.css'
+
+export default function Button({ id, label, onClick }) {
+    return (
+        <button className='btn' id={id} onClick={() => onClick}>
+            {label}
+        </button>
+    )
+}
