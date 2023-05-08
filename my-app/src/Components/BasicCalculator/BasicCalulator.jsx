99612034c4ded5283c1943163628431f3d82600b
Jake152 <jakepeterson1233@gmail.com>
2023-05-08 00:48:45 -0500

    Changes to BasicCalculator to handle changes to inputValue

--- my-app/src/Components/BasicCalculator/BasicCalulator.jsx
+++ my-app/src/Components/BasicCalculator/BasicCalulator.jsx
@@ -5,16 +5,29 @@ import ButtonContainer from './ButtonContainer/ButtonContainer';
 import './BasicCalculator.css'
 
 export default function BasicCalculator() {
-    const [inputValue, setInputValue] = useState('');
+    // Maybe do currentValue, calculationType, CalculationNumber, (On enter currentvalue changes based off calculation)
+    // const [calculationType, setCalculationType] = useState('');
+    const [inputValue, setInputValue] = useState(0);
 
-    useEffect(() => {
-        console.log(inputValue);
-    }, [inputValue]);
+    const handleChange = (calculationInput) => {
+        console.log(calculationInput);
+    }
+
+    const handleClearInput = () => {
+        setInputValue(0);
+    }
+
+    const handlePositiveNegative = () => {
+        if (inputValue) {
+            setInputValue(inputValue >= 0 ? -Math.abs(inputValue) : Math.abs(inputValue));
+        }
+      }
+      
 
     return (
         <div id='basic-calculator'>
-            <Input id='basic-calculator-input' type='text' inputValue={inputValue} setInputValue={setInputValue} />
-            <ButtonContainer />
+            <Input id='basic-calculator-input' type='number' inputValue={inputValue} setInputValue={setInputValue} />
+            <ButtonContainer handleChange={handleChange} handleClearInput={handleClearInput} handlePositiveNegative={handlePositiveNegative} />
         </div>
     )
 }
