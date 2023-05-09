afaeae1044f8b96987c4e5fbdf2434e96f173c1f
Jake152 <jakepeterson1233@gmail.com>
2023-05-08 21:29:43 -0500

    handleChange function added to button when clicked

--- my-app/src/Components/BasicCalculator/ButtonContainer/ButtonContainer.jsx
+++ my-app/src/Components/BasicCalculator/ButtonContainer/ButtonContainer.jsx
@@ -4,37 +4,37 @@ import Button from '../../../Common/Button/Button';
 
 import './ButtonContainer.css'
 
-export default function ButtonContainer({ handleChange, handleClearInput, handlePositiveNegative }) {
+export default function ButtonContainer({ handleCalculationChange, handleChange, handleClearInput, handlePositiveNegative, handleResult }) {
     return (
         <div id='btn-container'>
             <Row id='basic-calculator-row-1' className='basic-calculator-row'>
-                <Button id='clear-all-btn' label='Clear' onClick={() => handleClearInput(0)}/>
+                <Button id='clear-all-btn' label='Clear' onClick={() => handleClearInput()}/>
                 <Button id='postive-negative-btn' label='+/-' onClick={() => handlePositiveNegative()}/>
-                <Button id='percentage-btn' label='%' onClick={() => handleChange('%')}/>
-                <Button id='divide-btn' label='/' onClick={() => handleChange('/')}/>
+                <Button id='percentage-btn' label='%' onClick={() => handleCalculationChange('%')}/>
+                <Button id='divide-btn' label='/' onClick={() => handleCalculationChange('/')}/>
             </Row>
             <Row id='basic-calculator-row-2' className='basic-calculator-row'>
-                <Button id='num-seven-btn' label='7' onClick={() => handleChange(7)}/>
-                <Button id='num-eight-btn' label='8' onClick={() => handleChange(8)}/>
-                <Button id='num-nine-btn' label='9' onClick={() => handleChange(9)}/>
-                <Button id='multiplication-btn' label='X' onClick={() => handleChange('X')}/>
+                <Button id='num-seven-btn' label='7' onClick={() => handleChange('7')}/>
+                <Button id='num-eight-btn' label='8' onClick={() => handleChange('8')}/>
+                <Button id='num-nine-btn' label='9' onClick={() => handleChange('9')}/>
+                <Button id='multiplication-btn' label='X' onClick={() => handleCalculationChange('X')}/>
             </Row>
             <Row id='basic-calculator-row-3' className='basic-calculator-row'>
-                <Button id='num-four-btn' label='4' onClick={() => handleChange(4)}/>
-                <Button id='num-five-btn' label='5' onClick={() => handleChange(5)}/>
-                <Button id='num-six-btn' label='6' onClick={() => handleChange(6)}/>
-                <Button id='subtraction-btn' label='-' onClick={() => handleChange('-')}/>
+                <Button id='num-four-btn' label='4' onClick={() => handleChange('4')}/>
+                <Button id='num-five-btn' label='5' onClick={() => handleChange('5')}/>
+                <Button id='num-six-btn' label='6' onClick={() => handleChange('6')}/>
+                <Button id='subtraction-btn' label='-' onClick={() => handleCalculationChange('-')}/>
             </Row>
             <Row id='basic-calculator-row-4' className='basic-calculator-row'>
-                <Button id='num-one-btn' label='1' onClick={() => handleChange(1)}/>
-                <Button id='num-two-btn' label='2' onClick={() => handleChange(2)}/>
-                <Button id='num-three-btn' label='3' onClick={() => handleChange(3)}/>
-                <Button id='addition-btn' label='+' onClick={() => handleChange('+')}/>
+                <Button id='num-one-btn' label='1' onClick={() => handleChange('1')}/>
+                <Button id='num-two-btn' label='2' onClick={() => handleChange('2')}/>
+                <Button id='num-three-btn' label='3' onClick={() => handleChange('4')}/>
+                <Button id='addition-btn' label='+' onClick={() => handleCalculationChange('+')}/>
             </Row>
             <Row id='basic-calculator-row-5' className='basic-calculator-row'>
-                <Button id='num-0-btn' label='0' onClick={() => handleChange(0)}/>
-                <Button id='period-btn' label='.' onClick={() => handleChange('.')}/>
-                <Button id='equals-btn' label='=' onClick={() => handleChange('=')}/>
+                <Button id='num-0-btn' label='0' onClick={() => handleChange('0')}/>
+                <Button id='period-btn' label='.' onClick={() => handleCalculationChange('.')}/>
+                <Button id='equals-btn' label='=' onClick={() => handleResult('=')}/>
             </Row>
         </div>
     )
