780fb7c4ed322c5688b2f48c8209038df0f9da66
Jake Peterson <60277102+Jake152@users.noreply.github.com>
2023-05-08 22:54:09 -0500

    Workitem: App body and calculator width/height adjustment (#1)
    * Aligning items center in AppBody
    
    * Width/height change to MainContainer
    
    * Width change of equals button
    
    * Replaced MainContainer with BasicCalculator
    
    * Width/height/border styling for BasicCalculator component
    
    * Removing MainContainer component since its not necessary
    
    * min-width max-width so calculator stays same width no matter the zoom

--- my-app/src/Components/AppBody/AppBody.jsx
+++ my-app/src/Components/AppBody/AppBody.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import MainContainer from '../MainContainer/MainContainer';
+import BasicCalculator from '../BasicCalculator/BasicCalulator';
 
 import './AppBody.css'
 
 export default function AppBody() {
     return (
         <div id='app-body-container'>
-            <MainContainer />
+            <BasicCalculator />
         </div>
     )
 }
